d13b77d5787ea20083654415da100d58a5ed0cd9
Giuseppe <77988125+Rick-243@users.noreply.github.com>
2021-01-27 13:50:53 -0500

    Initial commit

---
+++ src/mundell/Main.java
@@ -0,0 +1,10 @@
+package mundell;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        String rickName = "Richard";
+        System.out.println("Hello my name is " + rickName);
+    }
+}

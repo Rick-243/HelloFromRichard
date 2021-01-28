4ab213e657dfebce4dcc63aa684f0b7019a7b93c
meflav <tru19006@byui.edu>
2021-01-28 00:23:06 -0500

    This is my reply...

--- src/mundell/Main.java
+++ src/mundell/Main.java
@@ -6,5 +6,10 @@ public class Main {
 	// write your code here
         String rickName = "Richard";
         System.out.println("Hello my name is " + rickName);
+
+    // This is my reply
+        String flavioName = "Flavio";
+        System.out.printf("Hello %s, my name is %s.%n", rickName, flavioName );
+
     }
 }
